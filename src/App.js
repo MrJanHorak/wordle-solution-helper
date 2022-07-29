c386fc19c77f6e552d0761a23b1a23f490a4c602
Jan Horak <janny_b_goode@hotmail.com>
2022-07-28 21:35:27 -0400

    stub up landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+
+import '../src/styles/App.css';
+import Landing from './pages/Landing';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <Landing />
       </header>
     </div>
   );
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Landing = () => {
+
+
+
+
+  return (
+    <>
+    The browser has landed
+    </>
+  )
+}
+
+export default Landing
