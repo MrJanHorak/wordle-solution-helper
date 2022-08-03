ff2b9ccd7b757da437e324e47ee59cb0d74c1c61
Jan Horak <janny_b_goode@hotmail.com>
2022-08-03 15:13:13 -0400

    add conditional to onClick for each row

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -17,9 +17,20 @@ const Landing = () => {
     5: ["", "", "", "", ""],
   });
 
+  const [letterArray, setLetterArray] = useState({
+    0: [[], [], [], [], []],
+    1: [[], [], [], [], []],
+    2: [[], [], [], [], []],
+    3: [[], [], [], [], []],
+    4: [[], [], [], [], []],
+    5: [[], [], [], [], []],
+  });
+
+  const[activeRow, setActiveRow] = useState(0)
+
   const handleClick = (e) => {
     console.log(e.target.id);
-   if(!isNaN(parseInt(e.target.id))){
+    if(!isNaN(parseInt(e.target.id))){
     let square = e.target.id.split("");
     let rowArray = squareClass[square[0]];
     rowArray[square[1]] === ""
@@ -39,7 +50,7 @@ const Landing = () => {
       <div className="tip-suggestion-container">
         <div className="tips">tips over here</div>
         <div className="grid-container">
-          <div className="row" id="row-one" onClick={handleClick}>
+          <div className="row" id="row-one" onClick={activeRow===0 ? handleClick : undefined}>
             <div className={`square ${squareClass[0][0]}`} id="00">
               W
             </div>
@@ -56,35 +67,35 @@ const Landing = () => {
               L
             </div>
           </div>
-          <div className="row" id="row-two" onClick={handleClick}>
+          <div className="row" id="row-two" onClick={activeRow===1 ? handleClick : undefined}>
             <div className={`square ${squareClass[1][0]}`} id="10"></div>
             <div className={`square ${squareClass[1][1]}`} id="11"></div>
             <div className={`square ${squareClass[1][2]}`} id="12"></div>
             <div className={`square ${squareClass[1][3]}`} id="13"></div>
             <div className={`square ${squareClass[1][4]}`} id="14"></div>
           </div>
-          <div className="row" id="row-three" onClick={handleClick}>
+          <div className="row" id="row-three" onClick={activeRow===2 ? handleClick : undefined}>
             <div className={`square ${squareClass[2][0]}`} id="20"></div>
             <div className={`square ${squareClass[2][1]}`} id="21"></div>
             <div className={`square ${squareClass[2][2]}`} id="22"></div>
             <div className={`square ${squareClass[2][3]}`} id="23"></div>
             <div className={`square ${squareClass[2][4]}`} id="24"></div>
           </div>
-          <div className="row" id="row-four" onClick={handleClick}>
+          <div className="row" id="row-four" onClick={activeRow===3 ? handleClick : undefined}>
             <div className={`square ${squareClass[3][0]}`} id="30"></div>
             <div className={`square ${squareClass[3][1]}`} id="31"></div>
             <div className={`square ${squareClass[3][2]}`} id="32"></div>
             <div className={`square ${squareClass[3][3]}`} id="33"></div>
             <div className={`square ${squareClass[3][4]}`} id="34"></div>
           </div>
-          <div className="row" id="row-five" onClick={handleClick}>
+          <div className="row" id="row-five" onClick={activeRow===4 ? handleClick : undefined}>
             <div className={`square ${squareClass[4][0]}`} id="40"></div>
             <div className={`square ${squareClass[4][1]}`} id="41"></div>
             <div className={`square ${squareClass[4][2]}`} id="42"></div>
             <div className={`square ${squareClass[4][3]}`} id="43"></div>
             <div className={`square ${squareClass[4][4]}`} id="44"></div>
           </div>
-          <div className="row" id="row-six" onClick={handleClick}>
+          <div className="row" id="row-six" onClick={activeRow===5 ? handleClick : undefined}>
             <div className={`square ${squareClass[5][0]}`} id="50"></div>
             <div className={`square ${squareClass[5][1]}`} id="51"></div>
             <div className={`square ${squareClass[5][2]}`} id="52"></div>
