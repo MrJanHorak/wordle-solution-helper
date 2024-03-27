22dcbb00fa5c45745c693009d93b363551eb501a
MrJanHorak <janny_b_goode@hotmail.com>
2024-03-27 18:42:16 -0400

    refactor Landing.jsx

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 // components
 import SuggestWords from "../components/SuggestWords";
@@ -55,7 +55,7 @@ const Landing = () => {
   const [message, setMessage] = useState("Enter your guess below.");
   const [mustNotContain, setMustNotContain] = useState([]);
   const [mustContain, setMustContain] = useState([]);
-  const [pastSolutions, setPastSolutions] = useState([])
+  const [pastSolutions, setPastSolutions] = useState([pastWordles])
 
   let atIndex = {};
   let inputArray = [];
@@ -90,46 +90,44 @@ const Landing = () => {
     return /^[a-zA-Z]{1,5}$/.test(str);
   };
 
-  const handleClick = (e) => {
+  const handleClick = useCallback((e) => {
     if (!isNaN(parseInt(e.target.id))) {
       let square = e.target.id.split("");
-      let rowArray = squareClass[square[0]];
-      rowArray[square[1]] === ""
-        ? (rowArray[square[1]] = "correct-letter")
-        : rowArray[square[1]] === "correct-letter"
-        ? (rowArray[square[1]] = "correct-place")
-        : (rowArray[square[1]] = "");
-      setSquareClass({ ...squareClass, [square[0]]: rowArray });
+      setSquareClass(prevSquareClass => {
+        let rowArray = [...prevSquareClass[square[0]]];
+        rowArray[square[1]] = rowArray[square[1]] === "" ? "correct-letter" : rowArray[square[1]] === "correct-letter" ? "correct-place" : "";
+        return { ...prevSquareClass, [square[0]]: rowArray };
+      });
     }
-  };
+  }, []);
 
-  const handleChange = (e) => {
+  const handleChange = useCallback((e) => {
     if (onlyLetters(e.target.value)) {
       setMessage("Enter your guess below.");
       inputArray = e.target.value.toUpperCase().split("");
       if (inputArray.length === 5) {
-        setRowFull({ ...rowFull, [activeRow]: true });
+        setRowFull(prevRowFull => ({ ...prevRowFull, [activeRow]: true }));
         setMessage("Click on letters Wordle has highlighted.");
       }
-      setLetterArray({ ...letterArray, [activeRow]: inputArray });
+      setLetterArray(prevLetterArray => ({ ...prevLetterArray, [activeRow]: inputArray }));
     } else {
       inputArray = [[], [], [], [], []];
-      setLetterArray({ ...letterArray, [activeRow]: inputArray });
+      setLetterArray(prevLetterArray => ({ ...prevLetterArray, [activeRow]: inputArray }));
       e.target.value = "";
       setMessage("Oops you entered a number!");
     }
-  };
+  }, [activeRow]);
 
-  const handleButton = (e) => {
+  const handleButton = useCallback((e) => {
     e.preventDefault();
     if (!rowFull[activeRow]) return;
     setMessage("Searching for possiblewords.");
     findSuggestions(letterArray[activeRow], squareClass[activeRow]);
-    setActiveRow(activeRow + 1);
+    setActiveRow(prevActiveRow => prevActiveRow + 1);
 
     if (activeRow === 5) setMessage("All rows are filled.");
     if (activeRow < 5) setMessage("Enter your next guess below.");
-  };
+  }, [activeRow, rowFull, letterArray, squareClass]);
 
   const findSuggestions = async (letters, priority) => {
     cleanedList = sourceWords;
@@ -170,235 +168,38 @@ const Landing = () => {
         </div>
         <div className="grid-container">
           <form>
-            <div
-              className="row"
-              id="row-one"
-              onClick={activeRow === 0 ? handleClick : undefined}
-            >
-              {activeRow === 0 && (
-                <div
-                  className={`letter-input ${
-                    rowFull[0] ? "rowfull" : undefined
-                  }`}
-                >
-                  <input
-                    type="text"
-                    name="0"
-                    maxLength={5}
-                    required
-                    autoComplete="off"
-                    onChange={activeRow === 0 ? handleChange : undefined}
-                    spellCheck="false"
-                    aria-label="input word"
-                  />
-                </div>
-              )}
-              <div className={`square ${squareClass[0][0]}`} id="00">
-                {letterArray[0][0]}
+            {Array.from({ length: 6 }, (_, i) => (
+              <div
+                key={i}
+                className="row"
+                id={`row-${i}`}
+                onClick={activeRow === i ? handleClick : undefined}
+              >
+                {activeRow === i && (
+                  <div
+                    className={`letter-input ${
+                      rowFull[i] ? "rowfull" : undefined
+                    }`}
+                  >
+                    <input
+                      type="text"
+                      name="0"
+                      maxLength={5}
+                      required
+                      autoComplete="off"
+                      onChange={activeRow === i ? handleChange : undefined}
+                      spellCheck="false"
+                      aria-label="input word"
+                    />
+                  </div>
+                )}
+                {Array.from({ length: 5 }, (_, j) => (
+                  <div className={`square ${squareClass[i][j]}`} id={`${i}${j}`} key={`${i}${j}`}>
+                    {letterArray[i][j]}
+                  </div>
+                ))}
               </div>
-              <div className={`square ${squareClass[0][1]}`} id="01">
-                {letterArray[0][1]}
-              </div>
-              <div className={`square ${squareClass[0][2]}`} id="02">
-                {letterArray[0][2]}
-              </div>
-              <div className={`square ${squareClass[0][3]}`} id="03">
-                {letterArray[0][3]}
-              </div>
-              <div className={`square ${squareClass[0][4]}`} id="04">
-                {letterArray[0][4]}
-              </div>
-            </div>
-            <div
-              className="row"
-              id="row-two"
-              onClick={activeRow === 1 ? handleClick : undefined}
-            >
-              {activeRow === 1 && (
-                <div
-                  className={`letter-input ${
-                    rowFull[1] ? "rowfull" : undefined
-                  }`}
-                >
-                  <input
-                    type="text"
-                    name="0"
-                    maxLength={5}
-                    required
-                    autoComplete="off"
-                    onChange={activeRow === 1 ? handleChange : undefined}
-                    spellCheck="false"
-                  />
-                </div>
-              )}
-              <div className={`square ${squareClass[1][0]}`} id="10">
-                {letterArray[1][0]}
-              </div>
-              <div className={`square ${squareClass[1][1]}`} id="11">
-                {letterArray[1][1]}
-              </div>
-              <div className={`square ${squareClass[1][2]}`} id="12">
-                {letterArray[1][2]}
-              </div>
-              <div className={`square ${squareClass[1][3]}`} id="13">
-                {letterArray[1][3]}
-              </div>
-              <div className={`square ${squareClass[1][4]}`} id="14">
-                {letterArray[1][4]}
-              </div>
-            </div>
-            <div
-              className="row"
-              id="row-three"
-              onClick={activeRow === 2 ? handleClick : undefined}
-            >
-              {activeRow === 2 && (
-                <div
-                  className={`letter-input ${
-                    rowFull[2] ? "rowfull" : undefined
-                  }`}
-                >
-                  <input
-                    type="text"
-                    name="0"
-                    maxLength={5}
-                    required
-                    autoComplete="off"
-                    onChange={activeRow === 2 ? handleChange : undefined}
-                    spellCheck="false"
-                  />
-                </div>
-              )}
-              <div className={`square ${squareClass[2][0]}`} id="20">
-                {letterArray[2][0]}
-              </div>
-              <div className={`square ${squareClass[2][1]}`} id="21">
-                {letterArray[2][1]}
-              </div>
-              <div className={`square ${squareClass[2][2]}`} id="22">
-                {letterArray[2][2]}
-              </div>
-              <div className={`square ${squareClass[2][3]}`} id="23">
-                {letterArray[2][3]}
-              </div>
-              <div className={`square ${squareClass[2][4]}`} id="24">
-                {letterArray[2][4]}
-              </div>
-            </div>
-            <div
-              className="row"
-              id="row-four"
-              onClick={activeRow === 3 ? handleClick : undefined}
-            >
-              {activeRow === 3 && (
-                <div
-                  className={`letter-input ${
-                    rowFull[3] ? "rowfull" : undefined
-                  }`}
-                >
-                  <input
-                    type="text"
-                    name="0"
-                    maxLength={5}
-                    required
-                    autoComplete="off"
-                    onChange={activeRow === 3 ? handleChange : undefined}
-                    spellCheck="false"
-                  />
-                </div>
-              )}
-              <div className={`square ${squareClass[3][0]}`} id="30">
-                {letterArray[3][0]}
-              </div>
-              <div className={`square ${squareClass[3][1]}`} id="31">
-                {letterArray[3][1]}
-              </div>
-              <div className={`square ${squareClass[3][2]}`} id="32">
-                {letterArray[3][2]}
-              </div>
-              <div className={`square ${squareClass[3][3]}`} id="33">
-                {letterArray[3][3]}
-              </div>
-              <div className={`square ${squareClass[3][4]}`} id="34">
-                {letterArray[3][4]}
-              </div>
-            </div>
-            <div
-              className="row"
-              id="row-five"
-              onClick={activeRow === 4 ? handleClick : undefined}
-            >
-              {activeRow === 4 && (
-                <div
-                  className={`letter-input ${
-                    rowFull[4] ? "rowfull" : undefined
-                  }`}
-                >
-                  <input
-                    type="text"
-                    name="0"
-                    maxLength={5}
-                    required
-                    autoComplete="off"
-                    onChange={activeRow === 4 ? handleChange : undefined}
-                    spellCheck="false"
-                  />
-                </div>
-              )}
-              <div className={`square ${squareClass[4][0]}`} id="40">
-                {letterArray[4][0]}
-              </div>
-              <div className={`square ${squareClass[4][1]}`} id="41">
-                {letterArray[4][1]}
-              </div>
-              <div className={`square ${squareClass[4][2]}`} id="42">
-                {letterArray[4][2]}
-              </div>
-              <div className={`square ${squareClass[4][3]}`} id="43">
-                {letterArray[4][3]}
-              </div>
-              <div className={`square ${squareClass[4][4]}`} id="44">
-                {letterArray[4][4]}
-              </div>
-            </div>
-            <div
-              className="row"
-              id="row-six"
-              onClick={activeRow === 5 ? handleClick : undefined}
-            >
-              {activeRow === 5 && (
-                <div
-                  className={`letter-input ${
-                    rowFull[5] ? "rowfull" : undefined
-                  }`}
-                >
-                  <input
-                    type="text"
-                    name="0"
-                    maxLength={5}
-                    required
-                    autoComplete="off"
-                    onChange={activeRow === 5 ? handleChange : undefined}
-                    spellCheck="false"
-                  />
-                </div>
-              )}
-              <div className={`square ${squareClass[5][0]}`} id="50">
-                {letterArray[5][0]}
-              </div>
-              <div className={`square ${squareClass[5][1]}`} id="51">
-                {letterArray[5][1]}
-              </div>
-              <div className={`square ${squareClass[5][2]}`} id="52">
-                {letterArray[5][2]}
-              </div>
-              <div className={`square ${squareClass[5][3]}`} id="53">
-                {letterArray[5][3]}
-              </div>
-              <div className={`square ${squareClass[5][4]}`} id="54">
-                {letterArray[5][4]}
-              </div>
-            </div>
+            ))}
             <div className="input-button">
               <button onClick={handleButton} aria-label="look for words">
                 Look for Words
@@ -440,6 +241,7 @@ const Landing = () => {
       </div>
     </div>
   );
-};
+
+}
 
 export default Landing;
